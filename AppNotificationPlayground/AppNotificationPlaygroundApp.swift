03c9d312438c963c28761b2d897f23d77d48a4a0
jeonghi <jpark0902@kookmin.ac.kr>
2025-03-04 10:37:53 +0900

    chore: 테스트 프로젝트 생성

---
+++ AppNotificationPlayground/AppNotificationPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNotificationPlaygroundApp.swift
+//  AppNotificationPlayground
+//
+//  Created by Lody on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AppNotificationPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
